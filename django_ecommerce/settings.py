3b9566450eaf0f2dfe519b46dcec85611b7aaa65
geera <geeralexandr2006@gmail.com>
2024-09-28 22:36:58 +0200

    ..

--- django_ecommerce/settings.py
+++ django_ecommerce/settings.py
@@ -32,7 +32,7 @@ DB_PORT = os.getenv('DB_PORT')
 DB_PASSWORD = os.getenv('DB_PASSWORD')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True if os.getenv('DEBUG') == 'True' else False
+DEBUG = os.getenv('DEBUG') == 'True'
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
 
